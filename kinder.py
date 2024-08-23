394312dc16d94ce95e49752d3500adcb6c7410bd
Jaem <derbenutzer999@gmail.com>
2024-08-23 15:14:12 +0500

    Add files via upload
    Feel free to check it out.

---
+++ kinder.py
@@ -0,0 +1,21 @@
+start = input('enter your age: ')
+end = input('enter your max age: ')
+years = int(end) - int(start)
+Interval = input('Input desired intervals: ')
+Count = input('Enter desired blyat, limit is 4: ')
+if int(Count) >=5 :
+	exit()	
+loss = 0
+
+for x in range(0, int(Count)):
+	factor = int(Interval)+x
+	temp = int(Count)-factor
+	loss = loss+temp
+
+
+
+print(loss)
+print(years*int(Count)-loss)
+
+
+
